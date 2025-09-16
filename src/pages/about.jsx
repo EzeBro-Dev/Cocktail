80f07d1e94bcb4ded7f86a274d6c92871cba3703
Elue Ezekiel <elueezekiel2005@gmail.com>
2025-09-16 13:39:44 +0100

    I added an about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
+import AboutUs from "../components/aboutUs"
 
 const About = () => {
   return (
     <>
         <Navbar />
+        <AboutUs />
         <Footer />
     </>
   )
